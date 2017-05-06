29313e0a7ac1c3c8c4ef0995cb5fa7c5948750af
newhuaszh <234615085@qq.com>
2017-05-06 17:32:23 +0800

    完成机构列表页的分页和筛选 2017-5-6 17:32:16

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'xadmin',
     'captcha',
+    'pure_pagination',
 ]
 
 AUTH_USER_MODEL = 'users.UserProfile'
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.shortcuts import render
 from django.views.generic import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import CourseOrg, CityDict
 
@@ -14,12 +15,49 @@ class OrgView(View):
     def get(self, request):
         # 课程机构
         all_orgs = CourseOrg.objects.all()
-        org_nums = all_orgs.count()
+
+        # 热门机构
+        hot_orgs = all_orgs.order_by('-click_nums')[:3]
+
         # 城市
         all_citys = CityDict.objects.all()
 
+        # 取出筛选城市
+        city_id_get = request.GET.get('city', '')
+        if city_id_get:
+            # 外键都可以通过 变量名 + _id 这种方式取得
+            all_orgs = all_orgs.filter(city_id=int(city_id_get))
+
+        # 类别筛选
+        category = request.GET.get('ct', '')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        org_nums = all_orgs.count()
+
+        # 进行排序
+        sort_type = request.GET.get('sort', '')
+        if sort_type == 'students':
+            all_orgs = all_orgs.order_by('-students')
+        elif sort_type == 'courses':
+            all_orgs = all_orgs.order_by('-course_nums')
+
+        # 对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_orgs, 5, request=request)
+
+        orgs = p.page(page)
+
         return render(request, 'org-list.html', {
-            'all_orgs': all_orgs,
+            'all_orgs': orgs,
             'all_citys': all_citys,
-            'org_nums': org_nums
+            'org_nums': org_nums,
+            'city_id':city_id_get,
+            'category': category,
+            'hot_orgs': hot_orgs,
+            'sort_type': sort_type,
         })
